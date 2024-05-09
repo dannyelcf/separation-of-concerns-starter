82cfeed9763d9d694f5447002cb93addf58d9bc8
Dannyel Cardoso da Fonseca <dannyelcf@gmail.com>
2024-05-09 20:44:30 +0200

    Add id into HTML and state

--- client/src/data/state.js
+++ client/src/data/state.js
@@ -1,18 +1,22 @@
 export const state = {
   todoList: [
     {
+      id: window.crypto.randomUUID(),
       done: false,
       description: 'Lean DOM',
     },
     {
+      id: window.crypto.randomUUID(),
       done: false,
       description: 'Lean Javascript',
     },
     {
+      id: window.crypto.randomUUID(),
       done: true,
       description: 'Lean CSS',
     },
     {
+      id: window.crypto.randomUUID(),
       done: true,
       description: 'Lean HTML',
     },
